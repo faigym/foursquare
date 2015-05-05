b9534c31998e53eb1d1bde4af17b83d4c74b0ed2
Olusesan <olusesan.ajina@gmail.com>
2015-05-04 18:00:44 -0700

    Initial Commit

---
+++ CoffeFix/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  CoffeFix
+//
+//  Created by Olusesan Ajina on 5/4/15.
+//  Copyright (c) 2015 Olusesan Ajina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
---
+++ CoffeFix/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoffeFix
+//
+//  Created by Olusesan Ajina on 5/4/15.
+//  Copyright (c) 2015 Olusesan Ajina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
